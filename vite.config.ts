0b6cdf43060f041914e832fee8febc58bc9c7a52
Razboy20 <razboy20@gmail.com>
2022-11-16 13:40:46 -0600

    add vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import vercel from "solid-start-vercel";
 import solid from "solid-start/vite";
 import Icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid(), Icons({ compiler: "solid" })],
+  plugins: [solid({ adapter: vercel() }), Icons({ compiler: "solid" })],
 });
