532a43fe69f2c05c15a8aae18133f7c64066403c
Razboy20 <razboy20@gmail.com>
2022-12-01 19:48:31 -0600

    add github link

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createSignal, JSX, Show } from "solid-js";
 import VideoPlayer from "~/components/VideoPlayer";
 import IconVideoFile from "~icons/material-symbols/video-file-rounded";
+import IconGithub from "~icons/mdi/github";
 
 export default function NosferatuPlayer() {
   const [videoBlob, setVideoBlob] = createSignal<File | null>(null);
@@ -21,17 +22,24 @@ export default function NosferatuPlayer() {
   let uploadRef: HTMLInputElement;
 
   return (
-    <main class="text-center p-4">
-      <Show when={!videoBlob()}>
-        <input type="file" accept="video/*" hidden ref={uploadRef} onChange={processFile} />
-        <button class="btn-upload flex items-center gap-1" onClick={() => uploadRef.click()}>
-          <IconVideoFile />
-          Select Video
-        </button>
-      </Show>
-      <Show when={videoURL()}>
-        <VideoPlayer src={videoURL()}></VideoPlayer>
-      </Show>
-    </main>
+    <>
+      <main class="text-center p-4">
+        <Show when={!videoBlob()}>
+          <input type="file" accept="video/*" hidden ref={uploadRef} onChange={processFile} />
+          <button class="btn-upload flex items-center gap-1" onClick={() => uploadRef.click()}>
+            <IconVideoFile />
+            Select Video
+          </button>
+        </Show>
+        <Show when={videoURL()}>
+          <VideoPlayer src={videoURL()}></VideoPlayer>
+        </Show>
+      </main>
+      <footer class="absolute bottom-0 w-full text-center text-gray-700 hover:text-gray-900 mb-2 p-1">
+        <a href="https://github.com/razboy20/nosferatu-player/" target="_blank" rel="noopener noreferrer">
+          <IconGithub class="inline -mt-0.5" /> Source
+        </a>
+      </footer>
+    </>
   );
 }
